0236081c8308fdc216555e39520a86baa48e3261
GadjahMada <ahmad210796@gmail.com>
2016-09-02 12:07:11 +0700

    Update veritrans.php

--- veritrans.php
+++ veritrans.php
@@ -1,95 +1,327 @@
 <?php
-require_once(dirname(__FILE__) . '/../../Veritrans.php');
-//Set Your server key
-Veritrans_Config::$serverKey = "VT-server-S2-RkQ0qYNOS55dr8AVRs6FU";
+/**
+ * Nonmerchant Demo Gateway
+ *
+ * @package blesta
+ * @subpackage blesta.components.gateways.veritrans
+ * @copyright Copyright (c) 2010, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+class Veritrans extends NonmerchantGateway {
+	/**
+	 * @var string The version of this gateway
+	 */
+	private static $version = "1.0.0";
+	/**
+	 * @var string The authors of this gateway
+	 */
+	private static $authors = array(array('name' => "Ahmad Khairun", 'url' => "https://www.neutronhost.net"));
+	/**
+	 * @var array An array of meta data for this gateway
+	 */
+	private $meta;
+	
+	
+	/**
+	 * Construct a new merchant gateway
+	 */
+	public function __construct() {
+		
+		// Load components required by this gateway
+		Loader::loadComponents($this, array("Input"));
+		
+		// Load the language required by this gateway
+		Language::loadLang("Veritrans", null, dirname(__FILE__) . DS . "language" . DS);
+	}
+	
+	/**
+	 * Returns the name of this gateway
+	 *
+	 * @return string The common name of this gateway
+	 */
+	public function getName() {
+		return Language::_("Veritrans.name", true);
+	}
+	
+	/**
+	 * Returns the version of this gateway
+	 *
+	 * @return string The current version of this gateway
+	 */
+	public function getVersion() {
+		return self::$version;
+	}
 
-// Uncomment for production environment
-// Veritrans_Config::$isProduction = true;
+	/**
+	 * Returns the name and URL for the authors of this gateway
+	 *
+	 * @return array The name and URL of the authors of this gateway
+	 */
+	public function getAuthors() {
+		return self::$authors;
+	}
+	
+	/**
+	 * Return all currencies supported by this gateway
+	 *
+	 * @return array A numerically indexed array containing all currency codes (ISO 4217 format) this gateway supports
+	 */
+	public function getCurrencies() {
+		return array("EUR", "GBP", "USD", "IDR";)
+	}
+	
+	/**
+	 * Sets the currency code to be used for all subsequent payments
+	 *
+	 * @param string $currency The ISO 4217 currency code to be used for subsequent payments
+	 */
+	public function setCurrency($currency) {
+		$this->currency = $currency;
+	}
+	
+	/**
+	 * Create and return the view content required to modify the settings of this gateway
+	 *
+	 * @param array $meta An array of meta (settings) data belonging to this gateway
+	 * @return string HTML content containing the fields to update the meta data for this gateway
+	 */
+	public function getSettings(array $meta=null) {
+		$this->view = $this->makeView("settings", "default", str_replace(ROOTWEBDIR, "", dirname(__FILE__) . DS));
 
-// Uncomment to enable sanitization
-// Veritrans_Config::$isSanitized = true;
+		// Load the helpers required for this view
+		Loader::loadHelpers($this, array("Form", "Html"));
 
-// Uncomment to enable 3D-Secure
-// Veritrans_Config::$is3ds = true;
+		$this->view->set("meta", $meta);
+		
+		return $this->view->fetch();
+	}
+	
+	/**
+	 * Validates the given meta (settings) data to be updated for this gateway
+	 *
+	 * @param array $meta An array of meta (settings) data to be updated for this gateway
+	 * @return array The meta data to be updated in the database for this gateway, or reset into the form on failure
+	 */
+	public function editSettings(array $meta) {
+		// Verify meta data is valid
+		$rules = array(
+			'key'=>array(
+				'valid'=>array(
+					'rule'=>array("betweenLength", 16, 16),
+					'message'=>Language::_("Veritrans.!error.key.valid", true)
+				)
+			)
+			
+			#
+			# 
+			
+		);
+		
+		$this->Input->setRules($rules);
+		
+		// Validate the given meta data to ensure it meets the requirements
+		$this->Input->validates($meta);
+		// Return the meta data, no changes required regardless of success or failure for this gateway
+		return $meta;
+	}
+	
+	/**
+	 * Returns an array of all fields to encrypt when storing in the database
+	 *
+	 * @return array An array of the field names to encrypt when storing in the database
+	 */
+	public function encryptableFields() {
+		
+		#
+		# TODO: return an array of all meta field names to store encrypted
+		#
+		
+		return array("key");
+	}
+	
+	/**
+	 * Sets the meta data for this particular gateway
+	 *
+	 * @param array $meta An array of meta data to set for this gateway
+	 */
+	public function setMeta(array $meta=null) {
+		$this->meta = $meta;
+	}
+	
+	/**
+	 * Returns all HTML markup required to render an authorization and capture payment form
+	 *
+	 * @param array $contact_info An array of contact info including:
+	 * 	- id The contact ID
+	 * 	- client_id The ID of the client this contact belongs to
+	 * 	- user_id The user ID this contact belongs to (if any)
+	 * 	- contact_type The type of contact
+	 * 	- contact_type_id The ID of the contact type
+	 * 	- first_name The first name on the contact
+	 * 	- last_name The last name on the contact
+	 * 	- title The title of the contact
+	 * 	- company The company name of the contact
+	 * 	- address1 The address 1 line of the contact
+	 * 	- address2 The address 2 line of the contact
+	 * 	- city The city of the contact
+	 * 	- state An array of state info including:
+	 * 		- code The 2 or 3-character state code
+	 * 		- name The local name of the country
+	 * 	- country An array of country info including:
+	 * 		- alpha2 The 2-character country code
+	 * 		- alpha3 The 3-cahracter country code
+	 * 		- name The english name of the country
+	 * 		- alt_name The local name of the country
+	 * 	- zip The zip/postal code of the contact
+	 * @param float $amount The amount to charge this contact
+	 * @param array $invoice_amounts An array of invoices, each containing:
+	 * 	- id The ID of the invoice being processed
+	 * 	- amount The amount being processed for this invoice (which is included in $amount)
+	 * @param array $options An array of options including:
+	 * 	- description The Description of the charge
+	 * 	- return_url The URL to redirect users to after a successful payment
+	 * 	- recur An array of recurring info including:
+	 * 		- amount The amount to recur
+	 * 		- term The term to recur
+	 * 		- period The recurring period (day, week, month, year, onetime) used in conjunction with term in order to determine the next recurring payment
+	 * @return string HTML markup required to render an authorization and capture payment form
+	 */
+	public function buildProcess(array $contact_info, $amount, array $invoice_amounts=null, array $options=null) {
+		$this->view = $this->makeView("process", "default", str_replace(ROOTWEBDIR, "", dirname(__FILE__) . DS));
 
-// Required
-$transaction_details = array(
-  'order_id' => rand(),
-  'gross_amount' => 145000, // no decimal allowed for creditcard
-  );
+		// Load the helpers required for this view
+		Loader::loadHelpers($this, array("Form", "Html"));
 
-// Optional
-$item1_details = array(
-    'id' => 'a1',
-    'price' => 50000,
-    'quantity' => 2,
-    'name' => "Apple"
-    );
+		$fields = array();
+		$post_to = "";
+		
+		#
+		# TODO: Define all form fields and the $post_to fields
+		#
 
-// Optional
-$item2_details = array(
-    'id' => 'a2',
-    'price' => 45000,
-    'quantity' => 1,
-    'name' => "Orange"
-    );
+		$this->view->set("post_to", $post_to);
+		$this->view->set("fields", $fields);
+		
+		return $this->view->fetch();
+	}
+	
+	/**
+	 * Validates the incoming POST/GET response from the gateway to ensure it is
+	 * legitimate and can be trusted.
+	 *
+	 * @param array $get The GET data for this request
+	 * @param array $post The POST data for this request
+	 * @return array An array of transaction data, sets any errors using Input if the data fails to validate
+	 *  - client_id The ID of the client that attempted the payment
+	 *  - amount The amount of the payment
+	 *  - currency The currency of the payment
+	 *  - invoices An array of invoices and the amount the payment should be applied to (if any) including:
+	 *  	- id The ID of the invoice to apply to
+	 *  	- amount The amount to apply to the invoice
+	 * 	- status The status of the transaction (approved, declined, void, pending, reconciled, refunded, returned)
+	 * 	- reference_id The reference ID for gateway-only use with this transaction (optional)
+	 * 	- transaction_id The ID returned by the gateway to identify this transaction
+	 */
+	public function validate(array $get, array $post) {
 
-// Optional
-$item_details = array ($item1_details, $item2_details);
+		#
+		# TODO: Verify the get/post data, then return the transaction
+		#
+		#
 
-// Optional
-$billing_address = array(
-    'first_name'    => "Andri",
-    'last_name'     => "Litani",
-    'address'       => "Mangga 20",
-    'city'          => "Jakarta",
-    'postal_code'   => "16602",
-    'phone'         => "081122334455",
-    'country_code'  => 'IDN'
-    );
-
-// Optional
-$shipping_address = array(
-    'first_name'    => "Obet",
-    'last_name'     => "Supriadi",
-    'address'       => "Manggis 90",
-    'city'          => "Jakarta",
-    'postal_code'   => "16601",
-    'phone'         => "08113366345",
-    'country_code'  => 'IDN'
-    );
-
-// Optional
-$customer_details = array(
-    'first_name'    => "Andri",
-    'last_name'     => "Litani",
-    'email'         => "andri@litani.com",
-    'phone'         => "081122334455",
-    'billing_address'  => $billing_address,
-    'shipping_address' => $shipping_address
-    );
-
-// Fill transaction details
-$transaction = array(
-    'transaction_details' => $transaction_details,
-    'customer_details' => $customer_details,
-    'item_details' => $item_details,
-    );
-
-try {
-  // Redirect to Veritrans VTWeb page
-  header('Location: ' . Veritrans_VtWeb::getRedirectionUrl($transaction));
+		// Log the successful response
+		$this->log($this->ifSet($_SERVER['REQUEST_URI']), serialize($post), "output", true);
+		
+		return array();
+	}
+	
+	/**
+	 * Returns data regarding a success transaction. This method is invoked when
+	 * a client returns from the non-merchant gateway's web site back to Blesta.
+	 *
+	 * @param array $get The GET data for this request
+	 * @param array $post The POST data for this request
+	 * @return array An array of transaction data, may set errors using Input if the data appears invalid
+	 *  - client_id The ID of the client that attempted the payment
+	 *  - amount The amount of the payment
+	 *  - currency The currency of the payment
+	 *  - invoices An array of invoices and the amount the payment should be applied to (if any) including:
+	 *  	- id The ID of the invoice to apply to
+	 *  	- amount The amount to apply to the invoice
+	 * 	- status The status of the transaction (approved, declined, void, pending, reconciled, refunded, returned)
+	 * 	- transaction_id The ID returned by the gateway to identify this transaction
+	 */
+	public function success(array $get, array $post) {
+		
+		#
+		# TODO: Return transaction data, if possible
+		#
+		
+		$this->Input->setErrors($this->getCommonError("unsupported"));
+	}
+	
+	/**
+	 * Captures a previously authorized payment
+	 *
+	 * @param string $reference_id The reference ID for the previously authorized transaction
+	 * @param string $transaction_id The transaction ID for the previously authorized transaction
+	 * @return array An array of transaction data including:
+	 * 	- status The status of the transaction (approved, declined, void, pending, reconciled, refunded, returned)
+	 * 	- reference_id The reference ID for gateway-only use with this transaction (optional)
+	 * 	- transaction_id The ID returned by the remote gateway to identify this transaction
+	 * 	- message The message to be displayed in the interface in addition to the standard message for this transaction status (optional)
+	 */
+	public function capture($reference_id, $transaction_id, $amount, array $invoice_amounts=null) {
+		
+		#
+		# TODO: Return transaction data, if possible
+		#
+		
+		$this->Input->setErrors($this->getCommonError("unsupported"));
+	}
+	
+	/**
+	 * Void a payment or authorization
+	 *
+	 * @param string $reference_id The reference ID for the previously submitted transaction
+	 * @param string $transaction_id The transaction ID for the previously submitted transaction
+	 * @param string $notes Notes about the void that may be sent to the client by the gateway
+	 * @return array An array of transaction data including:
+	 * 	- status The status of the transaction (approved, declined, void, pending, reconciled, refunded, returned)
+	 * 	- reference_id The reference ID for gateway-only use with this transaction (optional)
+	 * 	- transaction_id The ID returned by the remote gateway to identify this transaction
+	 * 	- message The message to be displayed in the interface in addition to the standard message for this transaction status (optional)
+	 */
+	public function void($reference_id, $transaction_id, $notes=null) {
+		
+		#
+		# TODO: Return transaction data, if possible
+		#
+		
+		$this->Input->setErrors($this->getCommonError("unsupported"));
+	}
+	
+	/**
+	 * Refund a payment
+	 *
+	 * @param string $reference_id The reference ID for the previously submitted transaction
+	 * @param string $transaction_id The transaction ID for the previously submitted transaction
+	 * @param float $amount The amount to refund this card
+	 * @param string $notes Notes about the refund that may be sent to the client by the gateway
+	 * @return array An array of transaction data including:
+	 * 	- status The status of the transaction (approved, declined, void, pending, reconciled, refunded, returned)
+	 * 	- reference_id The reference ID for gateway-only use with this transaction (optional)
+	 * 	- transaction_id The ID returned by the remote gateway to identify this transaction
+	 * 	- message The message to be displayed in the interface in addition to the standard message for this transaction status (optional)
+	 */
+	public function refund($reference_id, $transaction_id, $amount, $notes=null) {
+		
+		#
+		# TODO: Return transaction data, if possible
+		#
+		
+		$this->Input->setErrors($this->getCommonError("unsupported"));
+	}
 }
-catch (Exception $e) {
-  echo $e->getMessage();
-  if(strpos ($e->getMessage(), "Access denied due to unauthorized")){
-      echo "<code>";
-      echo "<h4>Please set real server key from sandbox</h4>";
-      echo "In file: " . __FILE__;
-      echo "<br>";
-      echo "<br>";
-      echo htmlspecialchars('Veritrans_Config::$serverKey = \'VT-server-S2-RkQ0qYNOS55dr8AVRs6FU\';');
-      die();
-}
-
-}
+?>
